3dbe61bd0a0b40ccc2198c4ad876b1edd51d7206
Shohin <Sokha.98@gmail.com>
2020-09-17 16:20:46 +0500

    добавлено Example

---
+++ pkg/bank/card/example_test.go
@@ -0,0 +1,28 @@
+package card
+
+import (
+	"bank/pkg/bank/types"
+	"fmt"
+)
+
+func ExamplePaymentSources() {
+	cards := []types.Card{
+		{
+			Balance: 100,
+			Active:  true,
+			PAN:     "1324",
+		},
+		{
+			Balance: 1001,
+			Active:  false,
+			PAN:     "13245",
+		},
+	}
+	res := PaymentSources(cards)
+
+	for _, c := range res {
+		fmt.Println(c.Number)
+	}
+
+	// Output: 1234
+}
